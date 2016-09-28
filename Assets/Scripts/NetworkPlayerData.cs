019c4eb40a206a215730e8e21fa2d19ce8abf236
pablomparada <pablo.paradabol@gmail.com>
2016-09-28 07:47:08 -0300

    Missing classes from last commit.

---
+++ Assets/Scripts/NetworkPlayerData.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine.Networking;
+
+public class NetworkPlayerData : MessageBase
+{
+    public int id;
+    public string name;
+    public int roomId;
+
+    public NetworkPlayerData() { }
+
+    public NetworkPlayerData(PlayerData playerData)
+    {
+        this.id = playerData.id;
+        this.name = playerData.playerName;
+        this.roomId = playerData.currentRoom.roomID;
+    }
+
+    public NetworkPlayerData(int id, string name, int roomId)
+    {
+        this.id = id;
+        this.name = name;
+        this.roomId = roomId;
+    }
+}
---
+++ Assets/Scripts/NetworkUpdateRecentJoinedPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Networking;
+
+public class NetworkUpdateRecentJoinedPlayer : MessageBase
+{
+    public NetworkPlayerData[] players;
+
+    public NetworkUpdateRecentJoinedPlayer() {}
+
+    public NetworkUpdateRecentJoinedPlayer(NetworkPlayerData[] players)
+    {
+        this.players = players;
+    }
+}
